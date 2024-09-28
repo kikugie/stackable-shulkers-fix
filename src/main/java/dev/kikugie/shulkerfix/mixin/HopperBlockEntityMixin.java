6adc7c46510745f7da08b3d7ab94b4d56bd1d7bf
WinExp <1450422389@qq.com>
2024-09-28 09:53:37 +0800

    clear comments

--- src/main/java/dev/kikugie/shulkerfix/mixin/HopperBlockEntityMixin.java
+++ src/main/java/dev/kikugie/shulkerfix/mixin/HopperBlockEntityMixin.java
@@ -34,33 +34,6 @@ public class HopperBlockEntityMixin {
 		return Util.isShulkerBox(instance) ? 1 : original.call(instance);
 	}
 
-	/*
-	@WrapOperation(
-		method = "extract(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/entity/ItemEntity;)Z",
-		at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/HopperBlockEntity;transfer(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/inventory/Inventory;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/math/Direction;)Lnet/minecraft/item/ItemStack;")
-	)
-	private static ItemStack modifyShulkerCount(Inventory from, Inventory to, ItemStack stack, Direction side, Operation<ItemStack> original) {
-		if (!ShulkerFixSettings.hopperCollectSingleShulkers || !Util.isShulkerBox(stack) || stack.getCount() <= 1)
-			return original.call(from, to, stack, side);
-		if (original.call(from, to, stack.copyWithCount(1), side).isEmpty())
-			stack.decrement(1);
-		return stack;
-	}
-
-	@Inject(
-		method = "extract(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/entity/ItemEntity;)Z",
-		at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z"),
-		cancellable = true
-	)
-	private static void checkIsShulker(Inventory inventory, ItemEntity itemEntity, CallbackInfoReturnable<Boolean> cir, @Local(ordinal = 1) ItemStack transferredStack) {
-		ItemStack stack = itemEntity.getStack();
-		if (ShulkerFixSettings.hopperCollectSingleShulkers && Util.isShulkerBox(stack) && stack.getCount() > 1) {
-			itemEntity.setStack(transferredStack);
-			cir.setReturnValue(true);
-		}
-	}
-	 */
-
 	@WrapOperation(
 		method = "extract(Lnet/minecraft/world/World;Lnet/minecraft/block/entity/Hopper;)Z",
 		at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/HopperBlockEntity;extract(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/entity/ItemEntity;)Z")
