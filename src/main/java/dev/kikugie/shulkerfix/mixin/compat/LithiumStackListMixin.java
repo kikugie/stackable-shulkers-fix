a37be9d11fea6a48afeaf8ce9a958f30dcec6b49
WinExp <1450422389@qq.com>
2024-10-30 17:24:18 +0800

    fix lithium 0.14 compatibility

--- src/main/java/dev/kikugie/shulkerfix/mixin/compat/LithiumStackListMixin.java
+++ src/main/java/dev/kikugie/shulkerfix/mixin/compat/LithiumStackListMixin.java
@@ -3,7 +3,7 @@ package dev.kikugie.shulkerfix.mixin.compat;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.kikugie.shulkerfix.Util;
-import me.jellysquid.mods.lithium.common.hopper.LithiumStackList;
+import net.caffeinemc.mods.lithium.common.hopper.LithiumStackList;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
@@ -16,7 +16,7 @@ public class LithiumStackListMixin {
 		method = "calculateSignalStrength",
 		at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getMaxCount()I")
 	)
-	private int get1(ItemStack instance, Operation<Integer> original) {
+	private int modifyShulkerMaxCount(ItemStack instance, Operation<Integer> original) {
 		return Util.isShulkerBoxLimited(instance) ? 1 : original.call(instance);
 	}
 }
