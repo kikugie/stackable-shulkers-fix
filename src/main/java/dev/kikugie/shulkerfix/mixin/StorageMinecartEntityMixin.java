25269e0ed6e1024069823029632381db471cfe21
kikugie <stormikiku@gmail.com>
2024-11-12 23:35:47 +0100

    Make StorageMinecartEntityMixin 1.21.3 compatible

--- src/main/java/dev/kikugie/shulkerfix/mixin/StorageMinecartEntityMixin.java
+++ src/main/java/dev/kikugie/shulkerfix/mixin/StorageMinecartEntityMixin.java
@@ -8,9 +8,23 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+/**
+ * Provides two method qualifiers for 1.21.3 compatibility.
+ */
 @Mixin(StorageMinecartEntity.class)
 public class StorageMinecartEntityMixin {
-	@ModifyArg(method = "applySlowdown", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/ScreenHandler;calculateComparatorOutput(Lnet/minecraft/inventory/Inventory;)I"))
+	@SuppressWarnings({"UnresolvedMixinReference", "UnnecessaryQualifiedMemberReference"})
+	@ModifyArg(
+		method = {
+			"Lnet/minecraft/entity/vehicle/StorageMinecartEntity;applySlowdown()V", // <1.21.2
+			"Lnet/minecraft/entity/vehicle/StorageMinecartEntity;applySlowdown(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;"
+		},
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/screen/ScreenHandler;calculateComparatorOutput(Lnet/minecraft/inventory/Inventory;)I"
+		),
+		require = 1
+	)
 	private Inventory fixMinecartSlowdown(Inventory instance) {
 		return ShulkerFixSettings.overstackedMinecartSlowdown ? instance : Util.wrapInventory(instance);
 	}
